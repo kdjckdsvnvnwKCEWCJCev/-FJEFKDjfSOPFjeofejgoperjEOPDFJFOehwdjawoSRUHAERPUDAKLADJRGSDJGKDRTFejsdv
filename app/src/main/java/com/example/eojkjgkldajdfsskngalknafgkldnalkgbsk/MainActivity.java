d94cf08cb917efdef9072688d0fb45b2a579f6ff
Dmitry Pudkov <57232605+hadouken900@users.noreply.github.com>
2021-01-25 22:37:49 +0300

    rename repo
    lexa rename

--- app/src/main/java/com/example/eojkjgkldajdfsskngalknafgkldnalkgbsk/MainActivity.java
+++ app/src/main/java/com/example/eojkjgkldajdfsskngalknafgkldnalkgbsk/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.eojkjgkldajdfsskngalknafgkldnalkgbsk;
+(◣_◢)
 
 import androidx.appcompat.app.AppCompatActivity;
+(◣_◢)
 
 import android.os.Bundle;
+(◣_◢)
 
 public class MainActivity extends AppCompatActivity {
-
+    (◣_◢)
+    //переименуй свой репозиторий
+    //или удали
+    //я его вижу каждый раз как захожу на гитхаб
+    //он меня раздражает
     @Override
+    (◣_◢)
     protected void onCreate(Bundle savedInstanceState) {
+        (◣_◢)
         super.onCreate(savedInstanceState);
+        (◣_◢)
         setContentView(R.layout.activity_main);
+        (◣_◢)
     }
+    (◣_◢)
 }
+(◣_◢)
